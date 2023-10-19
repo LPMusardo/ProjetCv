598ecb0f96b50fb346e4cb452a82df42aaa9eb29
LeoPaul <lpmusardo@gmail.com>
2023-10-19 17:21:25 +0200

    Ajout fonction pour get une NATURE random

--- src/main/java/com/example/projetcv/model/Nature.java
+++ src/main/java/com/example/projetcv/model/Nature.java
@@ -1,5 +1,19 @@
 package com.example.projetcv.model;
 
+import java.util.Random;
+
 public enum Nature {
-    EDUCATION, EXPERIENCE, PROJECT, OTHER
+    EDUCATION, EXPERIENCE, PROJECT, OTHER;
+
+    private static final Random random = new Random();
+
+
+    public static Nature randomNature()  {
+        Nature[] natures = values();
+        return natures[random.nextInt(natures.length)];
+    }
+
+
+
+
 }
