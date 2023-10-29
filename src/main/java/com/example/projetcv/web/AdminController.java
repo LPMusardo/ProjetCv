1f4c6b9bdd8e611421d9d28ebfa9225d163b072e
LeoPaul <lpmusardo@gmail.com>
2023-10-29 16:24:10 +0100

    add endpoints for all users

--- src/main/java/com/example/projetcv/web/AdminController.java
+++ src/main/java/com/example/projetcv/web/AdminController.java
@@ -1,15 +1,20 @@
 package com.example.projetcv.web;
 
+import com.example.projetcv.dto.SafeUserDto;
+import com.example.projetcv.model.User;
 import com.example.projetcv.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -28,9 +33,17 @@ public class AdminController {
     private ModelMapper modelMapper = new ModelMapper();
 
 
+    //-----------------------------------------------------------------------
+
     @GetMapping("/test")
-    public String refresh(HttpServletRequest req) {
-        return "Vous etes bien un ADMIN";
+    public String refresh(@AuthenticationPrincipal UserDetails userDetails) {
+        return "You are: "+userDetails.getAuthorities();
     }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(HttpServletRequest req) {
+        return userService.getAllUsers();
+    }
+
 
 }
