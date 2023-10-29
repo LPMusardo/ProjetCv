7e7de86eab05af61ffc6aa6ee276887daa2ef901
LeoPaul <lpmusardo@gmail.com>
2023-10-29 16:26:05 +0100

    wip

--- src/main/java/com/example/projetcv/web/CvController.java
+++ src/main/java/com/example/projetcv/web/CvController.java
@@ -4,10 +4,16 @@ import com.example.projetcv.model.Activity;
 import com.example.projetcv.model.CV;
 import com.example.projetcv.security.JwtHelper;
 import com.example.projetcv.service.CVService;
+import com.example.projetcv.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +24,17 @@ import java.util.logging.Logger;
 public class CvController {
 
     @Autowired
-    private JwtHelper jwtTokenProvider;
+    private UserService userService;
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private ModelMapper modelMapper = new ModelMapper();
+
     @Autowired
     private CVService cvService;
 
 
+    //-----------------------------------------------------------------------
 
     @GetMapping
     public List<CV> getAllCvs() {
@@ -30,41 +42,39 @@ public class CvController {
     }
 
     @GetMapping("/{id}")
-    public CV getCv(@PathVariable("id") Long id) {
+    public CV getCv(@PathVariable long id) {
         return cvService.getCvById(id);
 
     }
-// TODO :
+
+
 /*    @DeleteMapping
-    public String deleteCv() {
-        Long idUser = jwtTokenProvider.getId(jwtTokenProvider.resolveToken(req));
-        cvService.deleteCvByUserId(idUser);
-        return "You deleted  your Cv with id : " + idUser;
+    public ResponseEntity<String> deleteCv(@AuthenticationPrincipal UserDetails userDetails) {
+        cvService.deleteCvByUserId(userDetails.getUsername());
+        return new ResponseEntity<>("You deleted  your Cv with id : " + userDetails.getUsername(), HttpStatus.NO_CONTENT);
     }*/
 
-    @PostMapping
-    public String addActivity(HttpServletRequest req,@RequestBody Activity activity){
-        Long idUser = jwtTokenProvider.getId(jwtTokenProvider.resolveToken(req));
-        cvService.addActivityToCv(idUser , activity);
+
+/*    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public String addActivity(@AuthenticationPrincipal UserDetails userDetails, @RequestBody Activity activity){
+        cvService.addActivityToCv(userDetails.getUsername() , activity);
         return "activity added";
-    }
+    }*/
 
-    @PatchMapping("/{idActivity}")
+/*    @PatchMapping("/{idActivity}")
     public Activity updateCv(@PathVariable Long idActivity, @RequestBody Activity activity){
-
         return  cvService.updateActivity(idActivity , activity);
 
+    }*/
 
-    }
-
-    @DeleteMapping
-    public String deleteActivity(HttpServletRequest req,Long idActivity){
-        Long idUser = jwtTokenProvider.getId(jwtTokenProvider.resolveToken(req));
-
-        cvService.removeActivityToCv(idUser,idActivity);
+/*    @DeleteMapping
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public String deleteActivity(@AuthenticationPrincipal UserDetails userDetails, Long idActivity){
+        cvService.removeActivityToCv(userDetails.getUsername(),idActivity);
         return "";
 
-    }
+    }*/
 
 
 
