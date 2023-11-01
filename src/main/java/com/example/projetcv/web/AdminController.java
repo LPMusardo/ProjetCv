048a9a28fc082265de47e9619cfdba0ebba38ac0
LeoPaul <lpmusardo@gmail.com>
2023-11-01 19:56:56 +0100

    remove useless endpoint

--- src/main/java/com/example/projetcv/web/AdminController.java
+++ src/main/java/com/example/projetcv/web/AdminController.java
@@ -1,20 +1,14 @@
 package com.example.projetcv.web;
 
-import com.example.projetcv.dto.SafeUserDto;
-import com.example.projetcv.model.User;
+import com.example.projetcv.dto.UserSafeDto;
 import com.example.projetcv.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -30,7 +24,6 @@ public class AdminController {
 
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
-    private ModelMapper modelMapper = new ModelMapper();
 
 
     //-----------------------------------------------------------------------
@@ -41,13 +34,8 @@ public class AdminController {
     }
 
     @GetMapping("/users")
-    public SafeUserDto[] getAllUsers() {
+    public UserSafeDto[] getAllUsers() {
         return userService.getAllUsers();
-    }
-
-   @GetMapping("/users/page")
-    public Object getPageUsers(@PageableDefault(page = 0, size = 5) Pageable pageable) {
-        return userService.getPageUsers(pageable);
     }
 
 
