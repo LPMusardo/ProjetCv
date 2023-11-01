5ad10b0acf817456ad1e101a81a12ed08073352a
LeoPaul <lpmusardo@gmail.com>
2023-11-01 19:58:20 +0100

    remove useless modelMapper

--- src/main/java/com/example/projetcv/web/PublicController.java
+++ src/main/java/com/example/projetcv/web/PublicController.java
@@ -2,9 +2,6 @@ package com.example.projetcv.web;
 
 
 import com.example.projetcv.dao.UserRepository;
-import com.example.projetcv.model.User;
-import jakarta.servlet.http.HttpServletRequest;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -22,7 +18,6 @@ public class PublicController {
 
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
-    private ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     UserRepository userRepository;
