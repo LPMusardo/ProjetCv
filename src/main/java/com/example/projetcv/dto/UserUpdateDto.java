17b22feca6a92ece97bc2b544686e2ab18b84a94
LeoPaul <lpmusardo@gmail.com>
2023-11-17 22:05:15 +0100

    Fix : method name now start with "is..." otherwise it was not working

--- src/main/java/com/example/projetcv/dto/UserUpdateDto.java
+++ src/main/java/com/example/projetcv/dto/UserUpdateDto.java
@@ -13,27 +13,34 @@ import java.time.LocalDate;
 public class UserUpdateDto {
 
     /*null is valid*/
-    @Size(min = 2, message = "name is to short")
+    @Size(min = 2, message = "name is too short")
     private String name;
 
     /*null is valid*/
-    @Size(min = 2, message = "firstName is to short")
+    @Size(min = 2, message = "firstName is too short")
     private String firstName;
 
     /*null is valid*/
     @Email(message = "email must be correct")
-    @Size(min = 1, message = "email must be blank")
+    @Size(min = 1, message = "email must not be blank")
     private String email;
 
     private String website;
 
-    /*null is valid*/
+    /* because of the casting in LocalDate, if birthday is present, it must be valid*/
     private LocalDate birthday;
 
     private String password;
 
+    /*null is valid if password is null too*/
     private String passwordConfirm;
 
+
+
+    /******************************************************************
+     * METHOD NAME MUST BEGIN WITH "is" like in "isPasswordLongEnough"
+     * otherwise you will cry
+     /******************************************************************/
     /*null is valid*/
     @AssertTrue(message = "Password and password confirmation must match")
     public boolean isPasswordPresentAndConfirmed() {
@@ -41,8 +48,9 @@ public class UserUpdateDto {
         return password.equals(passwordConfirm);
     }
 
+    /*null is valid*/
     @AssertTrue(message = "Password must not be too short")
-    public boolean PasswordSize() {
+    public boolean isPasswordLongEnough() {
         if (password==null) return true;
         return password.length()>2 && password.equals(passwordConfirm);
     }
