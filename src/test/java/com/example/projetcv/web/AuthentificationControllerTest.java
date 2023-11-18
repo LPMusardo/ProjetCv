5bbc2e8dda363d2845fca004e9609f4bfcd98723
maximeGuiliani <maxime.guiliani@gmail.com>
2023-11-18 14:48:15 +0100

    Test Logout

--- src/test/java/com/example/projetcv/web/AuthentificationControllerTest.java
+++ src/test/java/com/example/projetcv/web/AuthentificationControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.projetcv.web;
 
 import com.example.projetcv.dto.LoginDto;
+import com.example.projetcv.security.JwtHelper;
 import com.example.projetcv.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,18 +13,28 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AuthentificationControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
     private UserService userService;
+
+    @MockBean
+    private JwtHelper jwtHelper;
 
     @Autowired
     private MockMvc mvc;
@@ -50,12 +62,44 @@ public class AuthentificationControllerTest {
         LoginDto loginDto = new LoginDto();
         loginDto.setEmail("max@gmail.com");
         loginDto.setPassword("pswd");
-
         given(userService.login(any(LoginDto.class))).willReturn("JWT");
         mvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("JWT"));
+    }
+
+    @Test
+    public void testLogoutNotConnected() throws Exception {
+        mvc.perform(post("/api/auth/logout")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(status().reason("Access Denied"));
+
+    }
+
+    @Test
+    @WithMockUser
+    public void testLogoutNoHeader() throws Exception {
+        mvc.perform(post("/api/auth/logout")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(status().reason(containsString("Required request body is missing")));
+                .andExpect(status().reason("Something went wrong"));
+
+    }
+
+
+    @Test
+    @WithMockUser
+    public void testLogoutConnected() throws Exception {
+        String expectedResponse = "Logout successful";
+        given(userService.logout(any(HttpServletRequest.class))).willReturn(expectedResponse);
+        mvc.perform(get("/api/auth/logout")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedResponse));
+
     }
 
 
