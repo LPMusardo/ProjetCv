6b151c794e258f306d402b1112ee369ae2ce7da5
maximeGuiliani <maxime.guiliani@gmail.com>
2023-11-25 16:37:14 +0100

    FIX : JWT bug

--- src/main/java/com/example/projetcv/security/JwtHelper.java
+++ src/main/java/com/example/projetcv/security/JwtHelper.java
@@ -6,6 +6,7 @@ import com.example.projetcv.model.User;
 import io.jsonwebtoken.*;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.xml.bind.DatatypeConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
@@ -46,9 +50,10 @@ public class JwtHelper {
     @Autowired
     private UserDetailsService userDetailsServiceImpl;
 
+
     @PostConstruct
     protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
 
@@ -61,30 +66,41 @@ public class JwtHelper {
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        byte[] decodedKey = DatatypeConverter.parseBase64Binary(secretKey);
+        SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
         String token = Jwts.builder()//
                 .setClaims(claims)//
                 .setIssuedAt(now)//
                 .setExpiration(validity)//
-                .signWith(SignatureAlgorithm.HS256, secretKey)//
+                .signWith(originalKey)// Use the SecretKey instance here
                 .compact();
-        if(useWhitelist) whitelist.addToken(token);
+        if (useWhitelist) whitelist.addToken(token);
         logger.info("token created " + token);
         return token;
     }
 
 
     public Authentication getAuthentication(String token) throws MyJwtException {
-        try{
+        try {
             UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(getUserId(token));
             return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MyJwtException("User from token not found in database", HttpStatus.UNAUTHORIZED);
         }
     }
 
 
     public String getUserId(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        byte[] decodedKey = DatatypeConverter.parseBase64Binary(secretKey);
+        SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
+        return Jwts.parserBuilder() // Use parserBuilder instead of parser
+                .setSigningKey(originalKey) // Use the SecretKey instance here
+                .build() // Build the parser
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
     }
 
 
@@ -95,10 +111,15 @@ public class JwtHelper {
     }
 
 
-    public boolean validateToken(String token) throws MyJwtException{
+    public boolean validateToken(String token) throws MyJwtException {
         logger.info("Before validate token: " + token);
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            byte[] decodedKey = DatatypeConverter.parseBase64Binary(secretKey);
+            SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
+            Jwts.parserBuilder() // use parserBuilder instead of parser
+                    .setSigningKey(originalKey) // use the SecretKey instance here
+                    .build() // create a parser instance by calling build
+                    .parseClaimsJws(token);
         } catch (Exception e) {
             logger.info("Invalid or expired token");
             throw new MyJwtException("Invalid or expired token", HttpStatus.UNAUTHORIZED);
@@ -114,7 +135,7 @@ public class JwtHelper {
 
 
     public String removeTokenFromWhiteList(String token) {
-        if(useWhitelist) whitelist.removeToken(token);
+        if (useWhitelist) whitelist.removeToken(token);
         return "token removed " + token;
     }
 
--- src/main/java/com/example/projetcv/security/JwtWhitelist.java
+++ src/main/java/com/example/projetcv/security/JwtWhitelist.java
@@ -1,56 +1,58 @@
 package com.example.projetcv.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
-
+import org.springframework.stereotype.Component;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.Logger;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @ConditionalOnProperty(name = "com.example.projetcv.jwt.use-whitelist", havingValue = "true")
 public class JwtWhitelist {
 
-    private final Logger logger = Logger.getLogger(JwtHelper.class.getName());
-
+    private final Logger logger = Logger.getLogger(JwtWhitelist.class.getName());
 
     @Value("${security.jwt.token.secret-key:SaS7FS7dvnMP1RwJT/zcTmLAUd07vICgJTzpB1MFtnY=}")
     private String secretKey;
 
-
     private final ArrayList<String> whiteList = new ArrayList<>();
-
 
     //-----------------------------------------------------------------------------------
 
-    public boolean addToken(String token){
-        return this.whiteList.add(token);
+    public void addToken(String token) {
+        this.whiteList.add(token);
     }
 
-    public boolean containsToken(String token){
+    public boolean containsToken(String token) {
         return this.whiteList.contains(token);
     }
 
-    public boolean removeToken(String token){
-        return this.whiteList.remove(token);
+    public void removeToken(String token) {
+        this.whiteList.remove(token);
     }
-
 
     public void cleanExpiredTokens() {
         logger.info("cleaning whitelist");
-        for (int i = 0; i < whiteList.size(); i++) {
+        Iterator<String> iterator = whiteList.iterator();
+        while (iterator.hasNext()) {
+            String token = iterator.next();
             try {
-                Jwts.parser().setSigningKey(secretKey).parseClaimsJws(whiteList.get(i)).getBody().getExpiration();
+                Jwts.parserBuilder()
+                        .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getExpiration();
             } catch (Exception e) {
-                whiteList.remove(i);
+                logger.info(e.getMessage());
+                iterator.remove();
             }
         }
         logger.info("whitelist size: " + whiteList.size());
     }
-
-
-
-
 }
